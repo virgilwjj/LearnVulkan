34ea8a9cee408aa750498bbc0052634cf926fd23
virgilwjj <virgilwjj@gmail.com>
2022-05-08 20:23:19 +0800

    add helloWindow

---
+++ build/demos/00_HelloWindow/CMakeFiles/HelloWindow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWindow.dir/main.cpp.o"
+  "CMakeFiles/HelloWindow.dir/main.cpp.o.d"
+  "HelloWindow"
+  "HelloWindow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/demos/00_HelloWindow/CMakeFiles/HelloWindow.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HelloWindow.
+# This may be replaced when dependencies are built.
